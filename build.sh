06917ba7842e7b6472eee63c1ed5a6b52a966b64
Daniel Neugebauer <dneuge@energiequant.de>
2016-07-24 19:10:58 +0200

    dynamically searching for Blackmagic SDK

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 basedir="$(dirname $(realpath $0))"
-decklink_dir="${basedir}/../../Downloads/Blackmagic DeckLink SDK 9.7.7/Linux/include"
+#decklink_dir="${basedir}/../../Downloads/Blackmagic DeckLink SDK 9.7.7/Linux/include"
 
-cd ${basedir} && rm -Rf build && mkdir build && cd build && cmake .. -DDECKLINK_INCLUDE_DIRS="${decklink_dir}" && make
+#cd ${basedir} && rm -Rf build && mkdir build && cd build && cmake .. -DDECKLINK_INCLUDE_DIRS="${decklink_dir}" && make
+cd ${basedir} && rm -Rf build && mkdir build && cd build && cmake .. && make
